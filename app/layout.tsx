c711a959614d35a58be22e9c1571de51d1d44d02
gto <rachid.boudjenane@gmail.com>
2023-10-28 19:18:14 +0200

    init projet + clerk auth 🔐

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,8 +16,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+      <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="flex min-h-screen w-full flex-col items-center dark:bg-black">
+      {children}
+      </div>
+      </body>
     </html>
+      </ClerkProvider>
   )
 }
